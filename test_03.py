81c8b71cd3e8e1a8d7bd45d02284232949b95603
Michael Crichlow <mikecrichlow@gmail.com>
2025-03-06 17:41:17 -0800

    Add files via upload

---
+++ test_03.py
@@ -0,0 +1,32 @@
+def is_prime(n: int) -> bool:
+    if n < 2:
+        return False
+    for val in range(2, (n // 2) + 1):
+        if n % val == 0:
+            return False
+
+    return True
+
+
+def get_prime_factors(n: int) -> list[int]:
+    # repeated divide by smallest prime number that divides evenly
+    # until you reach 1
+    _n = n
+    local_array = []
+    while _n != 1:
+        for val in range(2, _n + 1):
+            if is_prime(val):
+                if _n % val == 0:
+                    local_array.append(val)
+                    _n = _n // val
+                    break
+
+    return local_array
+
+
+def main() -> None:
+    print(get_prime_factors(17))
+
+
+if __name__ == '__main__':
+    main()
